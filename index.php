c51c276c9c2c6d6e5af0908c0c8d96222f93dca6
Thomas <thomas@thomas-mcdevitt.com>
2015-08-02 14:16:38 +0100

    stylesheet and script

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Personal Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+	<div>This is a page.
+	</div>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,3 @@
+$(html).ready(function() {
+	alert('ready');
+})
